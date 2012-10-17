a7d6151cecd9845d8837fc17be7ab3b81015e7d3
Roberto Bampi <bampi.roberto@gmail.com>
2012-10-17 13:07:53 +0200

    Setup file created. It is now installable via pip

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import wpsutils
+
+setup(
+    name = "wpsutils",
+    version = wpsutils.__version__,
+    description = open("README", 'r').read(),
+    url = "",
+    author="Roberto Bampi",
+    author_email="robampi@fbk.eu",
+    packages = [
+        "wpsutils"
+    ],
+    classifiers = [
+        'Programming Language :: Python',
+        #'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Indipendent',
+        'Framework :: Django',
+    ],
+    requires = [
+        'django-jsonfield',
+        'pytz'
+    ],
+    dependency_links = [
+        'https://github.com/davidek/django-tojson.git#egg=tojson',
+    ]
+)
+
+
